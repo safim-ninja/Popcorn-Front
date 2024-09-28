96e868b67b816f4dbd5b14b7826708ee5c9c21c4
Safim Md Fahim <safim.me@gmail.com>
2024-09-28 17:58:26 +0600

    axios

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 const Login = () => {
     const login = () => {
         localStorage.setItem('ACCESS_TOKEN', '1234');
@@ -9,9 +11,9 @@ const Login = () => {
             <section className="bg-gray-50 dark:bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                        <img className="w-8 h-8 mr-2"
-                             src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
-                        Flowbite
+                        <img className="w-8 h-8 mr-2" src={"src/Assets/popcorn.png"}
+                             alt="logo"/>
+                        PopCorn
                     </a>
                     <div
                         className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -56,9 +58,11 @@ const Login = () => {
                                     in
                                 </button>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet? <a href="#"
-                                                                  className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign
-                                    up</a>
+                                    Don’t have an account yet?
+                                    <Link to="/register"
+                                       className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                                        Sign up
+                                    </Link>
                                 </p>
                             </form>
                         </div>
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 const Register = () => {
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
@@ -56,9 +58,11 @@ const Register = () => {
                                 an account
                             </button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Already have an account? <a href="#"
-                                                            className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login
-                                here</a>
+                                Already have an account?
+                                <Link to="/login"
+                                      className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                                    Login here
+                                </Link>
                             </p>
                         </form>
                     </div>
